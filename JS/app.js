16e8925bc22dce9ae8bcfb0de1d4407bbafe9ebd
Miguel Carmona <miguelcarmona996@gmail.com>
2021-09-20 15:08:52 -0400

    Added js to filter buttons

--- JS/app.js
+++ JS/app.js
@@ -74,9 +74,19 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
+
+// Load items
 window.addEventListener('DOMContentLoaded', function(){
   displayMenuItems(menu);
+});
+
+// Filter Items
+filterBtns.forEach(function(){
+  btn.addEventListener('click', function(e){
+    console.log(e.currentTarget.dataset.id);
+  });
 });
 
 function displayMenuItems(menuItems){
